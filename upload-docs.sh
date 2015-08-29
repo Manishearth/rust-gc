856708d6ab6b8ea7e0a120ef5c74809b3eeeeff6
Manish Goregaokar <manishsmail@gmail.com>
2015-08-30 01:23:16 +0530

    travis should work again now

--- upload-docs.sh
+++ upload-docs.sh
@@ -2,7 +2,8 @@ set -e
 if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then 
   cargo doc
   sudo pip install ghp-import
-  ghp-import -n target/doc git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+  ghp-import -n target/doc
+  git push -qf https://${TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
   echo "Doc upload finished"
 fi
 
