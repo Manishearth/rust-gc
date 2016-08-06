30c367937332bcbf8361c8f2ac25a027166b70ff
Andrew Paseltiner <apaseltiner@gmail.com>
2016-08-06 15:37:15 -0400

    Use `Shared` to avoid aliasing `Box`es

--- gc/src/gc.rs
+++ gc/src/gc.rs
@@ -1,6 +1,6 @@
 use std::cell::{Cell, RefCell};
 use std::mem;
-use std::ptr::{self, Shared};
+use std::ptr::Shared;
 use trace::Trace;
 
 const INITIAL_THRESHOLD: usize = 100;
@@ -14,8 +14,20 @@ const USED_SPACE_RATIO: f64 = 0.7;
 struct GcState {
     bytes_allocated: usize,
     threshold: usize,
-    boxes_start: Option<Box<GcBox<Trace + 'static>>>,
-    boxes_end: *mut Option<Box<GcBox<Trace + 'static>>>,
+    boxes_start: Option<Shared<GcBox<Trace>>>,
+}
+
+impl Drop for GcState {
+    fn drop(&mut self) {
+        unsafe {
+            GC_SWEEPING.with(|collecting| collecting.set(true));
+
+            while let Some(node) = self.boxes_start {
+                let node = Box::from_raw(*node);
+                self.boxes_start = node.header.next;
+            }
+        }
+    }
 }
 
 /// Whether or not the thread is currently in the sweep phase of garbage collection.
@@ -27,14 +39,13 @@ thread_local!(static GC_STATE: RefCell<GcState> = RefCell::new(GcState {
     bytes_allocated: 0,
     threshold: INITIAL_THRESHOLD,
     boxes_start: None,
-    boxes_end: ptr::null_mut(),
 }));
 
 pub struct GcBoxHeader {
     // XXX This is horribly space inefficient - not sure if we care
     // We are using a word word bool - there is a full 63 bits of unused data :(
     roots: Cell<usize>,
-    next: Option<Box<GcBox<Trace + 'static>>>,
+    next: Option<Shared<GcBox<Trace>>>,
     marked: Cell<bool>,
 }
 
@@ -49,8 +60,8 @@ impl<T: Trace> GcBox<T> {
     ///
     /// A `GcBox` allocated this way starts its life rooted.
     pub fn new(value: T) -> Shared<Self> {
-        GC_STATE.with(|_st| {
-            let mut st = _st.borrow_mut();
+        GC_STATE.with(|st| {
+            let mut st = st.borrow_mut();
 
             // XXX We should probably be more clever about collecting
             if st.bytes_allocated > st.threshold {
@@ -64,36 +75,24 @@ impl<T: Trace> GcBox<T> {
                 }
             }
 
-            let mut gcbox = Box::new(GcBox {
-                header: GcBoxHeader {
-                    roots: Cell::new(1),
-                    marked: Cell::new(false),
-                    next: None,
-                },
-                data: value,
-            });
+            let gcbox = unsafe {
+                Shared::new(Box::into_raw(Box::new(GcBox {
+                    header: GcBoxHeader {
+                        roots: Cell::new(1),
+                        marked: Cell::new(false),
+                        next: st.boxes_start.take(),
+                    },
+                    data: value,
+                })))
+            };
 
-            let gcbox_ptr = unsafe { Shared::new(&mut *gcbox as *mut _) };
-
-            let next_boxes_end = &mut gcbox.header.next as *mut _;
-            if st.boxes_end.is_null() {
-                assert!(st.boxes_start.is_none(),
-                        "If something had been allocated, boxes_end would be set");
-                // The next place we're going to add something!
-                st.boxes_end = next_boxes_end;
-                st.boxes_start = Some(gcbox);
-            } else {
-                unsafe {
-                    *st.boxes_end = Some(gcbox);
-                }
-                st.boxes_end = next_boxes_end;
-            }
+            st.boxes_start = Some(gcbox);
 
             // We allocated some bytes! Let's record it
             st.bytes_allocated += mem::size_of::<GcBox<T>>();
 
             // Return the pointer to the newly allocated data
-            gcbox_ptr
+            gcbox
         })
     }
 }
@@ -132,62 +131,45 @@ impl<T: Trace + ?Sized> GcBox<T> {
         &self.data
     }
 
-    fn header_mut(&mut self) -> &mut GcBoxHeader {
-        &mut self.header
-    }
-
     fn size_of(&self) -> usize { mem::size_of_val(self) }
 }
 
 /// Collects garbage.
 fn collect_garbage(st: &mut GcState) {
-    let mut next_node = &mut st.boxes_start
-        as *mut Option<Box<GcBox<Trace + 'static>>>;
+    unsafe fn mark(mut head: Option<Shared<GcBox<Trace>>>) {
+        while let Some(node) = head {
+            if (**node).header.roots.get() > 0 {
+                (**node).trace_inner();
+            }
 
-    // Mark
-    while let Some(ref mut node) = *unsafe { &mut *next_node } {
-        {
-            let header = node.header_mut();
-            next_node = &mut header.next as *mut _;
-
-            // If it doesn't have roots - we can abort now
-            if header.roots.get() == 0 { continue }
-        }
-        // We trace in a different scope such that node isn't
-        // mutably borrowed anymore
-        unsafe { node.trace_inner(); }
-    }
-
-    GC_SWEEPING.with(|collecting| collecting.set(true));
-
-    let mut next_node = &mut st.boxes_start
-        as *mut Option<Box<GcBox<Trace + 'static>>>;
-
-    // Sweep
-    while let Some(ref mut node) = *unsafe { &mut *next_node } {
-        let size = node.size_of();
-        let header = node.header_mut();
-
-        if header.marked.get() {
-            // This node has already been marked - we're done!
-            header.marked.set(false);
-            next_node = &mut header.next;
-        } else {
-            // The node wasn't marked - we need to delete it
-            st.bytes_allocated -= size;
-            let mut tmp = None;
-            mem::swap(&mut tmp, &mut header.next);
-            mem::swap(&mut tmp, unsafe { &mut *next_node });
-
-            // At this point, the node is destroyed if it exists due to tmp dropping
+            head = (**node).header.next;
         }
     }
 
-    // Update the end pointer to point to the correct location
-    st.boxes_end = next_node;
+    unsafe fn sweep(mut head: &mut Option<Shared<GcBox<Trace>>>, bytes_allocated: &mut usize) {
+        GC_SWEEPING.with(|collecting| collecting.set(true));
 
-    // XXX This should probably be done with some kind of finally guard
-    GC_SWEEPING.with(|collecting| collecting.set(false));
+        while let Some(node) = *head {
+            if (**node).header.marked.get() {
+                // This node has already been marked - we're done!
+                (**node).header.marked.set(false);
+                head = &mut (**node).header.next;
+            } else {
+                // The node wasn't marked - we need to delete it
+                let mut node = Box::from_raw(*node);
+                *bytes_allocated -= node.size_of();
+                *head = node.header.next.take();
+            }
+        }
+
+        // XXX This should probably be done with some kind of finally guard
+        GC_SWEEPING.with(|collecting| collecting.set(false));
+    }
+
+    unsafe {
+        mark(st.boxes_start);
+        sweep(&mut st.boxes_start, &mut st.bytes_allocated);
+    }
 }
 
 /// Immediately triggers a garbage collection on the current thread.
