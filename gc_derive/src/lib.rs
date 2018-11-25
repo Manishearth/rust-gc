c02d0e5c10f5d55a56e5cda78e3044121d208362
Nika Layzell <nika@thelayzells.com>
2018-11-25 16:24:30 -0500

    Bump syn dependency

--- gc_derive/src/lib.rs
+++ gc_derive/src/lib.rs
@@ -4,14 +4,15 @@ extern crate syn;
 extern crate synstructure;
 #[macro_use]
 extern crate quote;
+extern crate proc_macro2;
 
 decl_derive!([Trace, attributes(unsafe_ignore_trace)] => derive_trace);
 
-fn derive_trace(mut s: synstructure::Structure) -> quote::Tokens {
-    s.filter(|bi| !bi.ast().attrs.iter().any(|attr| attr.name() == "unsafe_ignore_trace"));
+fn derive_trace(mut s: synstructure::Structure) -> proc_macro2::TokenStream {
+    s.filter(|bi| !bi.ast().attrs.iter().any(|attr| attr.path.is_ident("unsafe_ignore_trace")));
     let trace_body = s.each(|bi| quote!(mark(#bi)));
 
-    let trace_impl = s.unsafe_bound_impl("::gc::Trace", quote! {
+    let trace_impl = s.unsafe_bound_impl(quote!(::gc::Trace), quote! {
         #[inline] unsafe fn trace(&self) {
             #[allow(dead_code)]
             #[inline]
@@ -50,7 +51,7 @@ fn derive_trace(mut s: synstructure::Structure) -> quote::Tokens {
     // We also implement drop to prevent unsafe drop implementations on this
     // type and encourage people to use Finalize. This implementation will
     // call `Finalize::finalize` if it is safe to do so.
-    let drop_impl = s.unbound_impl("::std::ops::Drop", quote! {
+    let drop_impl = s.unbound_impl(quote!(::std::ops::Drop), quote! {
         fn drop(&mut self) {
             if ::gc::finalizer_safe() {
                 ::gc::Finalize::finalize(self);
@@ -66,6 +67,6 @@ fn derive_trace(mut s: synstructure::Structure) -> quote::Tokens {
 
 decl_derive!([Finalize] => derive_finalize);
 
-fn derive_finalize(s: synstructure::Structure) -> quote::Tokens {
-    s.unbound_impl("::gc::Finalize", quote!())
+fn derive_finalize(s: synstructure::Structure) -> proc_macro2::TokenStream {
+    s.unbound_impl(quote!(::gc::Finalize), quote!())
 }
